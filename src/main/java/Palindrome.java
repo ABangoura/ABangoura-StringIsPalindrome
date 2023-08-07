c1c23af2ae640dec247a53aa33d8c65262a59849
Almamy Bangoura <albangoura@aol.com>
2023-08-07 04:04:55 +0000

    Coding Challenge "String Is Palindrome".

--- src/main/java/Palindrome.java
+++ src/main/java/Palindrome.java
@@ -12,6 +12,16 @@ public class Palindrome {
      * @return true if str is a palindrome, false otherwise.
      */
     public boolean pal(String str){
-        return false;
+        // Using recursion, we can set up the 2 base cases first:
+        if(str.length() <= 1) { //  BASE CASE 1: if the string is one character or less...
+            return true; // ...return true.
+        } else if(str.charAt(0) != str.charAt(str.length() - 1)) { // BASE CASE 2: if the two endpoint characters are different...
+            return false; // ...return false.
+        } else {
+            // If we get to this point, we know that the two endpoint characters are at least the same.
+            // We can retrieve the substring between them, and recursively call pal() to check if the
+            // substring is a palindrome, till we get to a base case.
+            return pal(str.substring(1, str.length() - 1));
+        }
     }
 }
